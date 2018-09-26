fb6ff0d9393d1f2a4c2e0380ad92b05a544d8f08
hadronw <hadronw@qq.com>
2018-09-26 14:00:39 +0800

    add article

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -15,7 +15,6 @@ git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" master:master
 git push --force --quiet "https://hadronw:${Travis_co_token}@${CO_REF}" master:master
 git push --force --quiet "https://${Travis_gitlab_token}@${GL_REE}" master:master
 git push --force --quiet "https://xus7jRLyRQ8FAszUuxeR@gitlab.com/hadronw/hadronw.git" master:master
-git push --force --quiet "https://gitlab-ci-token:${Travis_gitlab_token}@${GL_REE}" master:master
-git push --force --quiet "https://oauth2:${Travis_gitlab_token}@${GL_REE}" master:master
+
 
 
