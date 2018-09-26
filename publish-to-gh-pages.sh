e9a2fe8cc6b316bf6d0bf6e01e882df8cfc35cae
hadronw <hadronw@qq.com>
2018-09-26 13:30:48 +0800

    add article

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -13,6 +13,8 @@ git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" master:master
 git push --force --quiet "https://hadronw:${Travis_co_token}@${CO_REF}" master:master
-git push --force  --quiet "https://${Travis_gitlab_token}@${GL_REE}" master:master
+#git push --force --quiet "https://${Travis_gitlab_token}@${GL_REE}" master:master
+git push --force --quiet "https://gitlab-ci-token:${Travis_gitlab_token}@{GL_REE}" master:master
+git push --force --quiet "https://oauth2:${Travis_gitlab_token}@{GL_REE}" master:master
 
 
